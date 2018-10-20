7bf9a0f706e8cad8a818b7aeca22786f94d72994
Chris Fotso <42123505+chrisfotso@users.noreply.github.com>
2018-10-20 12:37:10 -0500

    Update codewars-scramblies-5-kyu.js

--- Codewars/codewars-scramblies-5-kyu.js
+++ Codewars/codewars-scramblies-5-kyu.js
@@ -5,8 +5,9 @@ function scramble(str1, str2) {
   let result = '';
   for (let i = 0; i < str2.length; i++) {
     let index = str1.indexOf(str2[i]);
+    let letter = str1[index];
     if (index >= 0) {
-      result += str1[index];
+      result += letter;
       str1 = str1.slice(0, index) + str1.slice(index + 1);
     } else break;
   }
