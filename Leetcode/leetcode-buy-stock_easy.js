7ef62b0ab60b7fbd864c5f39e7fa069a4cd1ef04
Chris Fotso <chrisfotso@gmail.com>
2019-06-04 18:43:39 -0500

    Refactored max profit problem

--- Leetcode/leetcode-buy-stock_easy.js
+++ Leetcode/leetcode-buy-stock_easy.js
@@ -1,19 +1,21 @@
 // Say you have an array for which the ith element is the price of a given stock on day i.
 // If you were only permitted to complete at most one transaction (i.e., buy one and sell one share of the stock), design an algorithm to find the maximum profit.
 // Note that you cannot sell a stock before you buy one.
+//Example input: [7,1,5,3,6,4]
+//Expected output: 5
+//Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5
 
-var maxProfit = function(prices) {
-    let sortedPrices = [...prices].sort();
-    let minNumber;
-    let profit = 0;
-    for (let i = 0; i < sortedPrices.length; i++) {
-        minNumber = sortedPrices[i];
-        index = prices.indexOf(minNumber);
-        for (let j = index + 1; j < prices.length; j++) {
-            if (prices[j] - minNumber > profit) {
-                profit = prices[j] - minNumber;
-            }
-        }
+const maxProfit = function(prices) {
+  let min = prices[0],
+    result = 0;
+
+  for (let i = 1; i < prices.length; i++) {
+    const currNum = prices[i];
+    if (currNum < min) min = currNum;
+    else if (currNum - min > result) {
+      result = currNum - min;
     }
-    return profit;
+  }
+
+  return result;
 };
